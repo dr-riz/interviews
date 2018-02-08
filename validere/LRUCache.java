497788ed6055919feb0e7e7911b5ef4bcddd438d
dr-riz <vizvan@gmail.com>
2018-02-08 15:00:24 -0500

    experimenting with LinkedHashMap

--- validere/LRUCache.java
+++ validere/LRUCache.java
@@ -8,16 +8,24 @@ public class LRUCache {
         System.out.println("LRUCache");
 
 //		myCache = lruCache(cacheSize);
-		LinkedHashMap<Integer,Integer> myCache = lruCache(3);
-		System.out.println(myCache.size());
+		Map<Integer,String> cache = lruCache(3);
+		System.out.println(cache.size());
+		
+		cache.put(101,"Let us C");  
+   		cache.put(102, "Operating System");  
+   		cache.put(103, "Data Communication and Networking");  
+   		System.out.println("Values before remove: "+ cache);   
+
+		cache.put(104, "104");  
+		System.out.println("Values before remove: "+ cache);  
 
     }
 
-	public static <K,V> Map<K,V> lruCache(final int maxSize) {
+	public static Map<Integer,String> lruCache(final int maxSize) {
 		System.out.println("lruCache");
-    	return new LinkedHashMap<K, V>(maxSize*4/3, 0.75f, true) {
+    	return new LinkedHashMap<Integer,String>(maxSize*4/3, 0.75f, true) {
         	@Override
-        	protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        	protected boolean removeEldestEntry(Map.Entry<Integer,String> eldest) {
             	return size() > maxSize;
         	}
    		};
--- validere/README.md
+++ validere/README.md
@@ -4,6 +4,8 @@ Requirement:
 Design and implement a "least recently used" cache, which evicts the least recently used item. The cache should map from keys to values and be initialized with a max size. When it is full, it should evict the least recently used item. Please provide test cases.
 
 
+Assumptions:
+
 
 Outstanding:
 - packaging
@@ -14,3 +16,4 @@ Outstanding:
 
 References:
 [1] https://stackoverflow.com/questions/6398902/best-way-to-implement-lru-cache
+[2] https://www.javatpoint.com/java-linkedhashmap
